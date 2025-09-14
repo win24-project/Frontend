2413c6d921f6d0b19656c47f0d5c1143976831f6
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2025-09-14 17:25:13 +0200

    add structure and layout to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from "react-router";
+import Layout from './components/Layout.jsx'
+import './global/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Layout>
+        <App />
+      </Layout>
+    </BrowserRouter>
   </StrictMode>,
 )
