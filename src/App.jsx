09145de21a7ad7c97472886acd7fa34cb516ddca
Emma <emmaschonbeck7@gmail.com>
2025-09-18 11:34:58 +0200

    Added validation and redirection when successfully signing up

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path='/' element={<Home/>} />
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
   )
 }
---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,11 @@
+import styles from './LoginForm.module.css';
+
+const LoginForm = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default LoginForm
