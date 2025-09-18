37cf8e37ef5c29f7732024483d6c55aa08bb8e4f
Alexander <alexanderlahouel@hotmail.com>
2025-09-18 13:35:09 +0200

    added logout feature

---
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -0,0 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import styles from './LogoutButton.module.css';
+
+
+const LogoutButton = () => {
+
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token");
+
+const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    navigate("/login");
+};
+
+if (!token) {
+    return null;
+}
+  return (
+     <button className={styles.btn-logout} onClick={handleLogout}>
+      Sign out
+    </button>
+  );
+
+
+}
+
+export default LogoutButton
