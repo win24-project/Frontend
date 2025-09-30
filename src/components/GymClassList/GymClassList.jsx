ffe882efe58303fc4a7b74f71b776fbeae2053c7
Sebastian-Hult <sebastian.hult@utb.ecutbildning.se>
2025-09-30 13:58:21 +0200

    add button in gymClassList that opens modal to add gymclass, add auth to button to only show for admin. Add modal with mapped form.

--- src/components/GymClassList/GymClassList.jsx
+++ src/components/GymClassList/GymClassList.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import GymClassCard from '../GymClassCard/GymClassCard'
+import AddGymClassModal from '../GymClassModals/AddGymClassModal'
+import { useAuth } from '../Context/AuthContext'
 import styles from './GymClassList.module.css'
 
 const GymClassList = () => {
   const [classes, setClasses] = useState([])
+  const [isOpen, setIsOpen] = useState(false)
+  const { userRole } = useAuth();
 
   const getClass = async () => {
     const res = await fetch("https://group-project-gymclassservice-dfbzd5dza7cxdnd6.swedencentral-01.azurewebsites.net/api/get-all")
@@ -21,12 +25,18 @@ const GymClassList = () => {
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.heading}>Available Gym Classes</h2>
+      <div className={styles.flex1}>
+        <h2 className={styles.heading}>Available Gym Classes</h2>
+        {userRole === "Admin" && (<button className={styles.addButton} type="button" onClick={() => setIsOpen(!isOpen)}><span>Add Gym Class</span></button>) }
+        
+      </div>
+
       <div className={styles.cardGrid}>
         {
           classes.map(info => (<GymClassCard key={info.id} item={info} />))
         }
       </div>
+      <AddGymClassModal isOpen={isOpen} onClose={() => setIsOpen(false)} onGymClassAdded={getClass} />
     </div>
 
   )
